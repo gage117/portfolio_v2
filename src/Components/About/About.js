53ee4652e417ca9a1cb6611689f366f0defc16ff
Gage Eide <gageeide@gmail.com>
2020-06-30 23:04:16 -0500

    Started Skills div

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,6 +8,9 @@ function About() {
 			<p className='intro' data-aos='fade-in'>Hi, my name is Gage!</p>
 			<p className='intro-2' data-aos='fade-in'>{'I\'m'} a Full Stack Software Engineer specializing in React, Node, Postgres, and Express.</p>
 			<img src={headshot} className='headshot' alt='headshot' />
+			<div className='skills' data-aos='fade-in'>
+				
+			</div>
 		</section>
 	);
 }
