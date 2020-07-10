0c00ef001b8ade9da5ef3dbe7acb96d46cb22658
Gage Eide <gageeide@gmail.com>
2020-07-10 14:11:58 -0500

    Put fade-in on About container to avoid children not getting rendered correctly

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,17 +3,17 @@ import headshot from '../../Images/Headshot_cropped.jpg';
 
 function About() {
 	return (
-		<section id='About'>
-			<div className='bio' data-aos='fade-in'>
+		<section id='About' data-aos='fade-in'>
+			<div className='bio'>
 				<div>
 					<p className='intro'>Hi, my name is Gage!</p>
 					<p className='intro-2'>{'I\'m'} a Full Stack Software Engineer specializing in React, Node, Postgres, and Express.</p>
 				</div>
 				<img src={headshot} className='headshot' alt='headshot' />
 			</div>
-			<p className='skills-header' data-aos='fade-in'>Skills</p>
+			<p className='skills-header'>Skills</p>
 			<hr />
-			<div className='skills' data-aos='fade-in'>
+			<div className='skills'>
 				{
 					//? Icons
 					//? On hover or on click: expand icon, display text of technology name
