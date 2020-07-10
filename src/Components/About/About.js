1820ca0995ce897f33dfc9698bbe65494e6e47ac
Gage Eide <gageeide@gmail.com>
2020-07-10 13:07:28 -0500

    Added flex to bio

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,9 +4,13 @@ import headshot from '../../Images/Headshot_cropped.jpg';
 function About() {
 	return (
 		<section id='About'>
-			<p className='intro' data-aos='fade-in'>Hi, my name is Gage!</p>
-			<p className='intro-2' data-aos='fade-in'>{'I\'m'} a Full Stack Software Engineer specializing in React, Node, Postgres, and Express.</p>
-			<img src={headshot} className='headshot' alt='headshot' data-aos='fade-in' />
+			<div className='bio'>
+				<div>
+					<p className='intro' data-aos='fade-in'>Hi, my name is Gage!</p>
+					<p className='intro-2' data-aos='fade-in'>{'I\'m'} a Full Stack Software Engineer specializing in React, Node, Postgres, and Express.</p>
+				</div>
+				<img src={headshot} className='headshot' alt='headshot' data-aos='fade-in' />
+			</div>
 			<p className='skills-header' data-aos='fade-in'>Skills</p>
 			<hr />
 			<div className='skills' data-aos='fade-in'>
