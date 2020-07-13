c296c9fe077bee62ca95426596d830ea93f21b35
Gage Eide <gageeide@gmail.com>
2020-07-13 09:58:42 -0500

    Added credit to icons8 for icons

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -15,6 +15,7 @@ function Contact() {
 				<a href="https://www.linkedin.com/in/gage-eide/" rel='noopener noreferrer' target="_blank"><img src={linkedinIcon} alt="LinkedIn logo" /></a>
 				<a href="mailto:gageeide@gmail.com" rel='noopener noreferrer' target="_blank"><img src={gmailIcon} alt="Gmail logo" /></a>
 			</div>
+			<p className="credit">Icons by <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">icons8</a></p>
 		</footer>
 	);
 }
