00bfb50530d0b472d806022c3067a3a4e405c398
Gage Eide <gageeide@gmail.com>
2020-07-16 11:38:10 -0500

    Changed Project elements to articles

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -3,12 +3,12 @@ import React from 'react';
 function Project(props) {
 	const {name, screenshot, description, repos, techStack} = props.project;
 	return (
-		<div key={name} className='project'>
+		<article key={name} className='project'>
 			<p className='name'>{name.toLowerCase()}</p>
 			<img src={screenshot} alt={`${name} screenshot`} />
 			<p className='description'>{description}</p>
 			<hr />
-		</div>
+		</article>
 	);
 }
 
