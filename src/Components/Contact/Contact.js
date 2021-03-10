49bdc452ca66bcf0cea81f8b95fe7d285a89bb21
Gage Eide <gageeide@gmail.com>
2021-03-10 12:29:03 -0600

    Removed animation from Contact footer

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
 // Icons
 import githubIcon from '../../Images/icons8-github.svg';
 import linkedinIcon from '../../Images/icons8-linkedin.svg';
@@ -7,19 +6,17 @@ import gmailIcon from '../../Images/icons8-gmail.svg';
 
 function Contact() {
 	return (
-		<ScrollAnimation animateIn='slideInUp' duration={0.4}>
-			<footer id="Contact">
-				<div id="contact-header">
-					<p>Contact Me</p>
-				</div>
-				<div className="contact-links">
-					<a href="https://github.com/gage117" rel='noopener noreferrer' target="_blank"><img src={githubIcon} alt="Github logo" /></a>
-					<a href="https://www.linkedin.com/in/gage-eide/" rel='noopener noreferrer' target="_blank"><img src={linkedinIcon} alt="LinkedIn logo" /></a>
-					<a href="mailto:gageeide@gmail.com" rel='noopener noreferrer' target="_blank"><img src={gmailIcon} alt="Gmail logo" /></a>
-				</div>
-				<p className="credit">Icons by <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">icons8</a></p>
-			</footer>
-		</ScrollAnimation>
+		<footer id="Contact">
+			<div id="contact-header">
+				<p>Contact Me</p>
+			</div>
+			<div className="contact-links">
+				<a href="https://github.com/gage117" rel='noopener noreferrer' target="_blank"><img src={githubIcon} alt="Github logo" /></a>
+				<a href="https://www.linkedin.com/in/gage-eide/" rel='noopener noreferrer' target="_blank"><img src={linkedinIcon} alt="LinkedIn logo" /></a>
+				<a href="mailto:gageeide@gmail.com" rel='noopener noreferrer' target="_blank"><img src={gmailIcon} alt="Gmail logo" /></a>
+			</div>
+			<p className="credit">Icons by <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer">icons8</a></p>
+		</footer>
 	);
 }
 
