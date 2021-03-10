c51f7784d415ee57d5506fd5b0b1ed859a0abedd
Gage Eide <gageeide@gmail.com>
2021-03-10 13:18:12 -0600

    Adjusted spacing to ensure all three 'What I Do' sections show up on initial load

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,7 +9,7 @@ import databaseIcon from '../../Images/icons8-database-64.png';
 function About() {
 	return (
 		<section id="About">
-			<ScrollAnimation animateIn='fadeIn' animateOnce={true} duration={0.75}>
+			<ScrollAnimation animateIn='fadeIn' animateOnce={true} duration={0.75} offset={0}>
 				<section className='bio'>
 					<div className='intro-p-container'>
 						<p className='intro'>Hi, my name is Gage!</p>
@@ -24,7 +24,7 @@ function About() {
 			<section className='what-i-do'>
 				<p className='section-header'>What I Do</p>
 				<hr />
-				<ScrollAnimation animateIn='slideInLeft' animateOnce={true} duration={0.75}>
+				<ScrollAnimation animateIn='slideInLeft' animateOnce={true} duration={0.75} offset={0}>
 					<article>
 						<section className='wid-header'>
 							<img src={designIcon} alt='react' />
@@ -33,7 +33,7 @@ function About() {
 						<p className='wid-text'>Need help building or designing a website? I design sleek and accessible sites using <a href='https://www.figma.com/' target='blank' rel='noopener noreferrer'>Figma</a>, and can redesign old websites to bring a new look to it. I can build these sites in anything from my favorite framework <a href='https://reactjs.org/' target='blank' rel='noopener noreferrer'>React</a> to simple HTML depending on complexity.</p>
 					</article>
 				</ScrollAnimation>
-				<ScrollAnimation animateIn='slideInRight' animateOnce={true} duration={0.75}>
+				<ScrollAnimation animateIn='slideInRight' animateOnce={true} duration={0.75} offset={0}>
 					<article>
 						<section className='wid-header'>
 							<img src={apiIcon} alt='api' />
@@ -42,7 +42,7 @@ function About() {
 						<p className='wid-text'>I have a knack for building APIs, and love doing it too. I primarily work with <a href='https://nodejs.org/en/' target='blank' rel='noopener noreferrer'>Node</a> and <a href='https://expressjs.com/' target='blank' rel='noopener noreferrer'>Express</a> for building the API and have proficient experience deploying with <a href='https://www.heroku.com/' target='blank' rel='noopener noreferrer'>Heroku</a>.</p>
 					</article>
 				</ScrollAnimation>
-				<ScrollAnimation animateIn='slideInLeft' animateOnce={true} duration={0.75}>
+				<ScrollAnimation animateIn='slideInLeft' animateOnce={true} duration={0.75} offset={0}>
 					<article>
 						<section className='wid-header'>
 							<img src={databaseIcon} alt='databases' />
