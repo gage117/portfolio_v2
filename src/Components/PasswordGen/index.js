089461823a2af9ff9e7231238ae28fd119001c20
gage117 <57643501+gage117@users.noreply.github.com>
2021-12-19 18:20:44 -0600

    Started implementation of state in password form

--- src/Components/PasswordGen/index.js
+++ src/Components/PasswordGen/index.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function handleInputChange(input, event) {
+  console.log( event.target );
+}
 
 export default function PasswordGen() {
+  const [password, setPassword] = useState('');
+  const [formObj, setFormObj] = useState({
+    length: 16,
+    lowercase: true,
+    uppercase: true,
+    numbers: true,
+    symbols: true,
+  });
+
   return (
     <main className='PasswordGen'>
       <form className='generatorForm'>
         <h1>I'm gonna generate you a password so hard</h1>
         <div className="inputDiv">
           <label for="length">Length</label>
-          <input type="text" id="length" value="16" />
+          <input type="text" id="length" value={formObj.length} onClick={(e) => handleInputChange("length", e)} />
         </div>
         <div className="inputDiv">
           <label for="lowercase">Lowercase</label>
-          <input type="checkbox" id="lowercase" checked />
+          <input type="checkbox" id="lowercase" checked={formObj.lowercase} onClick={(e) => handleInputChange("lowercase", e)} />
         </div>
         <div className="inputDiv">
           <label for="uppercase">Uppercase</label>
-          <input type="checkbox" id="uppercase" checked />
+          <input type="checkbox" id="uppercase" checked={formObj.uppercase} onClick={(e) => handleInputChange("uppercase", e)} />
         </div>
         <div className="inputDiv">
           <label for="numbers">Numbers</label>
-          <input type="checkbox" id="numbers" checked />
+          <input type="checkbox" id="numbers" checked={formObj.numbers} onClick={(e) => handleInputChange("numbers", e)} />
         </div>
         <div className="inputDiv">
           <label for="symbols">Symbols</label>
-          <input type="checkbox" id="symbols" checked />
+          <input type="checkbox" id="symbols" checked={formObj.symbols} onClick={(e) => handleInputChange("symbols", e)} />
         </div>
         <button>Generate</button>
         <div id="password"></div>
